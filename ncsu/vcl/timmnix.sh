e378dfcfd67990404607d47cd2498e637fced826
timm <timm@ieee.org>
2019-04-27 15:03:33 -0400

    saving

--- ncsu/vcl/timmnix.sh
+++ ncsu/vcl/timmnix.sh
@@ -4,46 +4,55 @@
 get="sudo apt-get -qq -y "
 set -x
 
+files() {
+  for f in  dot dotbashrc dottmux dotvimrc  tmux-session1 ; do
+    if [ ! -f "$f" ]; then
+      wget https://raw.githubusercontent.com/timm/dot/master/ncsu/vcl/$f
+    fi
+  done
+}
+
 os() {
-	$get update
-	$get upgrade
+  $get update
+  $get upgrade
 }
 
 lots() {
-	$get install aspell clisp ctags gnuplot gnu-smalltalk htop luajit lua5.2 mc ncdu python-pip 
-	sudo -H pip install --upgrade pip
-	$get install python3 swi-prolog tmux tree wget
-	# lit programming stuff
-	sudo -H pip install pycco
+  $get install aspell clisp ctags gnuplot gnu-smalltalk htop luajit lua5.2 mc ncdu python-pip 
+  sudo -H pip install --upgrade pip
+  $get install python3 swi-prolog tmux tree wget
+  # lit programming stuff
+  sudo -H pip install pycco
 }
 
 fun() {
-	$get  install bsdgames cmatrix
-	sudo add-apt-repository  -y ppa:ytvwld/asciiquarium
-	$get update 
-	$get install asciiquarium
+  $get  install bsdgames cmatrix
+  sudo add-apt-repository  -y ppa:ytvwld/asciiquarium
+  $get update 
+  $get install asciiquarium
 }
 
 bashing() {
-	Bashrc=$Dot/dotbashrc
-	There=$HOME/.bashrc
-	(grep ". $Bashrc" $There || echo ". $Bashrc" >> $There )>/dev/null
+  Bashrc=$Dot/dotbashrc
+  There=$HOME/.bashrc
+  (grep ". $Bashrc" $There || echo ". $Bashrc" >> $There )>/dev/null
 }
 
 vim8() {
-	sudo add-apt-repository  -y ppa:jonathonf/vim
-	$get update
-	$get upgrade vim
+  sudo add-apt-repository  -y ppa:jonathonf/vim
+  $get update
+  $get upgrade vim
 }
 
 vundle() {
-	mkdir -p $Dot/vim/bundle
-	mkdir -p $Dot/tmp
-	cd $Dot/vim/bundle
-	git clone https://github.com/gmarik/Vundle.vim.git
-	vim +PluginInstall +qall 
+  mkdir -p $Dot/vim/bundle
+  mkdir -p $Dot/tmp
+  cd $Dot/vim/bundle
+  git clone https://github.com/gmarik/Vundle.vim.git
+  vim +PluginInstall +qall 
 }
 
+files
 os
 lots
 fun
